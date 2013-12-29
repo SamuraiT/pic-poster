6c9327be6343a5b4db746a0fbbe80d6015e5a9a5
SamuraiT <t.yasukawa01@gmail.com>
2013-12-29 13:55:26 +0900

    add tweeting functions/codes so when you sumbit tweet from form, you can tweet

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,6 +1,6 @@
 from flask import request, redirect, url_for, render_template, flash
 from flaskr  import app
-
+from picbot.manage import Photographer
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -8,14 +8,19 @@ def index():
 @app.route('/tweet/',methods = ['POST'])
 def tweet():
     if request.method == 'POST':
+        raspi = Photographer()
         hastag = request.form['hastag']
+        comment = request.form['comment']
         if not hastag:
+            raspi.tweet(comment)
             flash('Cheers ! you tweeted using #asahikawa_python')
         elif not '#' in hastag:
+            raspi.tweet(comment,('#'+hastag))
             #tweet('#'+hastag)
             flash('Congrats! you tweeted using #{hastag}!'.format(
                 hastag = hastag))
         else:
+            raspi.tweet(comment,hastag)
             flash('Congrats! you tweeted !')
     return redirect(url_for('index'))
 
