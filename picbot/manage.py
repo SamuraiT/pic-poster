16c880669ee3fd991184ebb7987efad9e4569773
SamuraiT <t.yasukawa01@gmail.com>
2013-12-31 16:14:31 +0900

    fix type and add default hashtag

--- picbot/manage.py
+++ picbot/manage.py
@@ -2,7 +2,8 @@
 import pygame
 import pygame.camera
 from pygame.locals import *
-from picbot.settings import api
+from picbot.settings import api,HASH_TAG
+
 import sys
 class Photographer(object):
     def __init__(self,camera_port = "/dev/video0"):
@@ -17,28 +18,28 @@ class Photographer(object):
         self.filename = filename
         self.cam.stop()
 
-    def post_a_pic(self,comment,hastag = u'#asahikawa_python'):
+    def post_a_pic(self,comment,hashtag = HASH_TAG):
         photo = open(self.filename,'rb')
-        if len(comment+hastag) > 139:
+        if len(comment+hashtag) > 139:
             print 'your tweet is beyond 140 characters which'
             ' is the limitation of tweet.\n please tweet again'
             return None
         api.update_status_with_media(media=photo,
-                status=u'{comment} {hastag}'.format(
+                status=u'{comment} {hashtag}'.format(
                     comment = comment,
-                    hastag = hastag
+                    hashtag = hashtag
                     ))
 
-    def tweet(self,comment=u'',hastag = u'#asahikawa_python',
+    def tweet(self,comment=u'',hashtag = HASH_TAG,
             filename = 'pic.jpg'):
         self.take_a_pic(filename)
-        self.post_a_pic(comment,hastag)
+        self.post_a_pic(comment,hashtag)
 
 def main(*argv):
     Rpi = Photographer()
     if len(argv) == 3:
         Rpi.tweet(comment = argv[0][1],
-                hastag = argv[0][2])
+                hashtag = argv[0][2])
     elif len(argv) == 2:
         Rpi.tweet(comment = argv[0][1])
 
--- picbot/settings-sample.py
+++ picbot/settings-sample.py
@@ -7,6 +7,10 @@ ACCESS_SECRET = ''
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
 
+
+#This is default hashtag
+HASH_TAG = u'#asahikawa_python'
+
 if __name__ == '__main__':
     api.update_status(status=sys.argv[1])
 
